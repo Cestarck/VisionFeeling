804287dbc5962e3835fd37e371b76d1feda21b77
Cestarck <hernandezv.cesar@gmail.com>
2018-07-31 21:44:45 -0500

    logica camara

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
-
+  window.onload=function(){
+    var player = document.getElementById('player'); 
+    var snapshotCanvas = document.getElementById('snapshot');
+    var captureButton = document.getElementById('capture');
+    var videoTracks;
+  
+    var handleSuccess = function(stream) {
+      // Attach the video stream to the video element and autoplay.
+      player.srcObject = stream;
+      videoTracks = stream.getVideoTracks();
+    };
+  
+    captureButton.addEventListener('click', function() {
+      var context = snapshot.getContext('2d');
+      // frame to camvas.
+      context.drawImage(player, 0, 0, snapshotCanvas.width, 
+          snapshotCanvas.height);
+          // Stop stream
+      videoTracks.forEach(function(track) {track.stop()});
+    });
+  
+  
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(handleSuccess);
+}
 }, false);
